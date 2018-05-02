5f3a1dce48f753b3f4aa2507962a4e25a0cec35f
Centallas <centallas01@gmail.com>
2018-05-02 02:57:58 -0500

    Add project files.

---
+++ Empleado.Service/EmpleadoService.cs
@@ -0,0 +1,29 @@
+﻿using EmpleadoApp.Service.Interface;
+using EmpleadoAppRepository.Interface;
+using EmpleadoApp.Model;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+
+namespace EmpleadoApp.Service
+{
+    public class EmpleadoService : IEmpleadoService
+    {
+        private IEmpleadoRepository __repo;
+
+        public EmpleadoService(IEmpleadoRepository repo)
+        {
+
+            this.__repo = repo;
+        }
+        public async Task<List<Empleadoes>> Get()
+        {
+            return await __repo.Get();
+        }
+        public async Task<bool> Add(Empleadoes empleado)
+        {
+            return await __repo.Add(empleado);
+        }
+
+    }
+}
---
+++ Repository/Interface/IEmpleadoRepository.cs
@@ -0,0 +1,15 @@
+﻿using EmpleadoApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpleadoAppRepository.Interface
+{
+    public interface IEmpleadoRepository
+    {
+        Task<List<Empleadoes>> Get();
+        Task<bool> Add(Empleadoes empleado);
+    }
+}
